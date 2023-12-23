714fd95dda98c83b3e11bd93af2b7d8a10f50f88
Ahmadek <mazcuud010@gmail.com>
2023-12-23 18:28:32 +0300

    safe area context is updated and the android emulator is working fine now

--- Login.js
+++ Login.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { ScrollView } from "react-native";
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 const Login = ({ navigation }) => {
   return (
     <View style={styles.container}>
+
       <Image style={styles.image} source={require("./assets/login.jpg")} />
       <Text style={styles.text}>Login Screen</Text>
 
@@ -18,6 +21,7 @@ const Login = ({ navigation }) => {
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
+
     </View>
   );
 };
--- Navigation.js
+++ Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from "./Login"
+import Login from "./Login";
 import HomeScreen from './HomeScreen';
 import DetailsScreen from './DetailsScreen';
 import 'react-native-gesture-handler';
